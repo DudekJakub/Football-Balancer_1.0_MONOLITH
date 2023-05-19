06481dd4fd9568b70161f6d8908137fa11ace354
jDudek <jakub.dudek94@gmail.com>
2023-05-19 18:43:13 +0200

    class name changed

--- src/test/java/com/dudek/footballbalancer/mapper/FieldLocationMapperImplTests.java
+++ src/test/java/com/dudek/footballbalancer/mapper/FieldLocationMapperImplTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FieldLocationMapperImplTest {
+class FieldLocationMapperImplTests {
 
     private final FieldLocationMapperImpl mapper = new FieldLocationMapperImpl();
 
