3d35c558a3a54ef6fa3069edb5fde388d3b71c47
jDudek <jakub.dudek94@gmail.com>
2023-05-25 15:17:34 +0200

    SwaggerConfig update

--- src/main/java/com/dudek/footballbalancer/config/swagger/SwaggerConfig.java
+++ src/main/java/com/dudek/footballbalancer/config/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.dudek.footballbalancer.config.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +14,15 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI api() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Authentication", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .info(new Info()
                         .title("Monolith Service API")
                         .description("This API provides access to basic app resources, such as ROOM/PLAYER/SKILL etc.")
-                        .version("0.7.0"));
+                        .version("0.5.0"))
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"));
     }
 }
